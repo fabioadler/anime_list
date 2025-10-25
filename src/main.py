c746fe7e3a8814d760db9e950f5567429deb30c9
F <adlerluna@hotmail.com>
2025-10-25 14:07:47 -0300

    V 0.1.1 Buscando animes em um novo site

--- src/main.py
+++ src/main.py
@@ -2,128 +2,145 @@ from bs4 import BeautifulSoup as bs
 from PIL import Image
 from io import BytesIO
 import flet as ft
-import requests,base64,webbrowser,time
+import requests,base64,time
 
-class MeusAnimes():
+class AnimeList():
     def __init__(self,page:ft.Page):
         self.page=page
-        self.page.title="Animes"
+        self.page.title="AnimeList"
         self.page.window.icon="favicon.ico"
+        self.cor_vermelha="#BE0B32"
+        self.cor_branca="#ffffff"
+        self.cor_preta="#000000"
+        self.page.bgcolor=self.cor_preta
+        self.page.padding=0
+        self.page.spacing=0
         self.page.scroll=ft.ScrollMode.ALWAYS
-        self.page.update()
-        self.home()
-
-    def home(self):
-        self.page.clean()
-        self.animes=[]
-        for anime in self.get_animes():
-            self.animes.append(
+        self.get_animes()
+        self.list_anime =[]
+        for anime in self.animes:
+            ani = anime.split("|")
+            self.list_anime.append(
                 ft.Container(
-                    expand=True,
                     ink=True,
-                    url=anime["link"],
+                    url=ani[0],
+                    padding=10,
+                    height=260,
                     col={
-                        "xs":12,
-                        "sm":6,
+                        "xs":11,
                         "md":3
                     },
-                    padding=6,
-                    border_radius=10,
+                    bgcolor=self.cor_vermelha,
+                    border_radius=20,
+                    tooltip=ani[2],
                     content=ft.Column(
                         expand=True,
                         alignment=ft.MainAxisAlignment.CENTER,
-                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        spacing=0,
                         controls=[
-                            ft.Image(
-                                expand=True,
-                                border_radius=10,
-                                src_base64=self.image_web(anime["image"]),
-                                fit=ft.ImageFit.COVER
+                            ft.ResponsiveRow(
+                                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                                controls=[
+                                    ft.Container(
+                                        col={
+                                            "md":12
+                                        },
+                                        border_radius=20,
+                                        content=ft.Image(
+                                            src_base64=self.image_web(ani[1]),
+                                            fit=ft.ImageFit.COVER,
+                                            height=180
+                                        )
+                                    )
+                                ]
                             ),
-                            ft.Text(
-                                expand=True,
-                                value=anime["nome"]
+                            ft.ResponsiveRow(
+                                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                                controls=[
+                                    ft.Container(
+                                        col={
+                                            "md":12
+                                        },
+                                        padding=ft.Padding(top=10,bottom=0,left=0,right=0),
+                                        content=ft.Text(
+                                            value=ani[2][0:60],
+                                            text_align=ft.TextAlign.CENTER
+                                        )
+                                    )
+                                ]
                             )
                         ]
-                    ),
-                    on_click=lambda e: webbrowser.open_new(e.url)
+                    )
                 )
             )
-        self.conteudo_pag=ft.Container(
+        self.conteudo = ft.Column(
+            width=self.page.width,
             expand=True,
-            margin=ft.Margin(top=20,bottom=0,left=0,right=0),
-            content=ft.Column(
-                expand=True,
-                alignment=ft.MainAxisAlignment.CENTER,
-                controls=[
-                    ft.Container(
-                        expand=True,
-                        content=ft.ResponsiveRow(
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.START,
-                            controls=[
-                                ft.Container(
-                                    expand=True,
-                                    col={
-                                        "xs":10,
-                                        "sm":8,
-                                        "md":6
-                                    },
-                                    content=ft.Column(
-                                        expand=True,
-                                        alignment=ft.MainAxisAlignment.START,
-                                        controls=[
-                                            ft.Text(
-                                                value="Lista de Animes",
-                                                size=40,
-                                                weight=ft.FontWeight.BOLD
-                                            ),
-                                            ft.Text(
-                                                value="Animes Online",
-                                                size=15,
-                                                weight=ft.FontWeight.BOLD
-                                            )
-                                        ]
-                                    )
-                                )
-                            ]
+            spacing=0,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                ft.ResponsiveRow(
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                    controls=[
+                        ft.Container(
+                            padding=ft.Padding(top=10,bottom=10,left=0,right=0),
+                            col={
+                                "xs":6,
+                                "md":4
+                            },
+                            content=ft.Text(
+                                value="AnimeList",
+                                text_align=ft.TextAlign.CENTER,
+                                weight=ft.FontWeight.BOLD,
+                                size=40,
+                                color=self.cor_branca,
+                            )
+                        ),
+                        ft.Container(
+                            padding=ft.Padding(top=10,bottom=10,left=0,right=0),
+                            col={
+                                "xs":2,
+                                "md":1
+                            },
+                            content=ft.Image(
+                                src="favicon.png"
+                            )
                         )
-                    ),
-                    ft.Container(
-                        expand=True,
-                        margin=ft.Margin(top=40,bottom=0,left=0,right=0),
-                        content=ft.ResponsiveRow(
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.CENTER,
-                            controls=self.animes
-                        )
+                    ]
+                ),
+                ft.Container(
+                    col={
+                        "md":10
+                    },
+                    padding=ft.Padding(top=40,bottom=10,left=40,right=40),
+                    content=ft.ResponsiveRow(
+                        alignment=ft.MainAxisAlignment.START,
+                        vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                        controls=self.list_anime
                     )
-                ]
-            )
+                )
+            ]
         )
-        self.page.add(self.conteudo_pag)
+        self.page.add(
+            self.conteudo
+        )
         self.page.update()
-        time.sleep(60)
-        self.home()  
 
     def get_animes(self):
-        conteudo=(bs(requests.get("https://animesonlinecc.to/").text,"html.parser")).find_all("article",{'class':'item se episodes'})
-        animes=[]
-        c=0
-        for ani in list(map(lambda e: f"{e.text};;{str(e.find("img")).split('"')[3]};;{str(e.find("a")).split('"')[1]}",conteudo)):
-            if(c>5):
-                anime = ani.split(";;")
-                animes.append({"nome":anime[0],"image":anime[1],"link":anime[2]})
-            else:
-                pass
-            c+=1
+        self.url="https://www.anroll.net"
+        self.pag=bs(requests.get(self.url).text,"html.parser")
+        self.conteudo=self.pag.find_all("li",{"class":"sc-bpSLYx fRpZuV release-item"})
+        self.animes=[]
+        for c in self.conteudo:
+            a=str(c.find("a")).split('"')[1]
+            img=str(c.find("img")).split('>')[0].split('"')[len(str(c.find("img")).split('"'))-6].split(",")[0].replace("&amp;w=256&amp;q=75 1x","&w=384&q=75").replace("&amp;w=256&amp;q=75 2x","&w=384&q=75")
+            self.animes.append(f"{self.url}{a}|{self.url}{img}|{(c.text).replace("Episódio"," Epiósido")}")
 
-        return animes
-    
     def image_web(self,url):
-        png_output = BytesIO()
-        (Image.open(BytesIO(requests.get(url).content))).save(png_output, format="PNG")
+        png_output=BytesIO()
+        (Image.open(BytesIO(requests.get(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}).content))).save(png_output, format="PNG")
         png_output.seek(0)
         return base64.b64encode(png_output.read()).decode('utf-8')
     
-ft.app(target=MeusAnimes,assets_dir="assets")
+ft.app(target=AnimeList,assets_dir="assets")
